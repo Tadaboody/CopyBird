17e04eca8345a0bb25d2a08e8493f06f0e113ca5
Tadaboody <tomer.keren.dev@gmail.com>
2019-12-07 17:45:11 +0200

    Bind the pipes to two editable points instead of hyperparams

--- Wall.gd
+++ Wall.gd
@@ -3,19 +3,19 @@ extends Node2D
 export var window_height : int
 onready var screen_size = get_viewport().size
 onready var Pipe = preload("res://Pipe.tscn")
-
-# Called when the node enters the scene tree for the first time.
-func make_pipe(window_center: float,flipped:bool):
+	
+# Not that kind of pipe
+func make_pipe(window_center: float, flipped:bool):
 	var pipe : StaticBody2D = Pipe.instance()
 	if flipped:
 		pipe.rotate(deg2rad(180))
 	var coeff = 1 if not flipped else -1
 	pipe.position.y = window_center + (window_height)*coeff
 	return pipe
-	
+
 func _ready():
 	randomize()
-	var window_center = screen_size.y * clamp(randf(), 0.2, 0.8)
+	var window_center = lerp($WindowRoof.position.y, $WindowFloor.position.y, randf())
 	var upperPipe = make_pipe(window_center, false)
 	add_child(upperPipe)
 	var lowerPipe = make_pipe(window_center, true)
