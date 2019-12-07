1242435a650618bf93f894f400d1989c4ee2c9d6
Tomer Keren <tomer.keren.dev@gmail.com>
2019-12-07 15:47:43 +0200

    Get rid of ready() in favor of onready vars

--- Player.gd
+++ Player.gd
@@ -5,10 +5,8 @@ export var horizontal_speed : int
 export var gravity :int
 export var jump_speed : int
 export var rotation_modifier : int
-var velocity = Vector2()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	velocity.x = horizontal_speed
+
+onready var velocity = Vector2(horizontal_speed, 0)
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("jump"):
