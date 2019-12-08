8c4cff59fb49862782084ee57c2ecd841b2e10c0
Tomer Keren <tomer.keren.dev@gmail.com>
2019-12-08 22:59:47 +0200

    Edit window height in game scene

--- Game.gd
+++ Game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var Wall = preload("res://Wall.tscn")
 export var wallSpeed : float
+export var window_height : float
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_pipe_spawn()
@@ -15,4 +16,5 @@ func _pipe_spawn():
 	var wall = Wall.instance()
 	wall.position.x = get_viewport_rect().size.x
 	wall.speed = -wallSpeed
+	wall.window_height = window_height
 	add_child(wall)
