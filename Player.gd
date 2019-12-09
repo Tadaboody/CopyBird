611089d34c2122f261bf138cdc71e5b1f7ac14e8
Tomer Keren <tomer.keren.dev@gmail.com>
2019-12-09 19:43:02 +0200

    Add a max speed to player

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ export var horizontal_speed : int
 export var jump_speed : int
 export var fall_speed : int
 export var rotation_modifier : int
+export var max_speed : int
 
 onready var velocity = Vector2(horizontal_speed, 0)
 
@@ -15,6 +16,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump"):
 		velocity.y -= jump_speed
 	velocity.y += fall_speed * delta
+	velocity.y = clamp(velocity.y, -max_speed, max_speed)
 	position += velocity
 	position.y = clamp(position.y, 0, get_viewport().size.y)
 	$Sprite.rotation_degrees = clamp(velocity.y * rotation_modifier, -90, 90)
