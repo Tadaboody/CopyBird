0c0a48f83db71ef7deaa4cc986f4f687210760f8
Tomer Keren <tomer.keren.dev@gmail.com>
2019-12-14 11:16:48 +0200

    Make variant window height

--- Game.gd
+++ Game.gd
@@ -12,7 +12,7 @@ func _pipe_spawn():
 	var wall = Wall.instance()
 	wall.position.x = get_viewport_rect().size.x
 	wall.speed = -wallSpeed
-	wall.window_height = window_height
+	wall.window_height = rand_range(window_height/2,window_height*1.5)
 	$Walls.add_child(wall)
 
 func _on_Player_hit():
